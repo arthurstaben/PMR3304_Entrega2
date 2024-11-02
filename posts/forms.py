d7b43eaeeda1fcd4a20488a6b55a2316a3107a8c
João Arthur <arthurstaben@gmail.com>
2024-11-02 17:08:20 -0300

    CRUD - Versão 2

---
+++ posts/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Post
+
+# class PostForm(forms.Form):
+#     title = forms.CharField(label='Clube', max_length=100)
+#     year = forms.IntegerField(label='Ano', min_value=1857, max_value=2024)
+#     conteudo = forms.CharField(label='Descrição', max_length=1500)
+#     goleiro = forms.CharField(label='Goleiro', max_length=100)
+#     defesa = forms.CharField(label='Defesa', max_length=300)
+#     meio = forms.CharField(label='Meio-Campo', max_length=300)
+#     ataque = forms.CharField(label='Ataque', max_length=300)
+#     poster_url = forms.URLField(label='URL do Poster', max_length=600)
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'year',
+            'conteudo',
+            'goleiro',
+            'defesa',
+            'meio',
+            'ataque',
+            'poster_url',
+        ]
+        labels = {
+            'name': 'Clube',
+            'year': 'Ano',
+            'conteudo': 'Conteúdo',
+            'goleiro': 'Goleiro',
+            'defesa': 'Defesa',
+            'meio': 'Meio-Campo',
+            'ataque': 'Ataque',
+            'poster_url': 'URL do Poster',
+        }
--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 def detail_elenco(request, Post_id):
     post = get_object_or_404(Post, id=Post_id)
@@ -18,37 +19,56 @@ def list_elencos(request):
 
 def create_elenco(request):
     if request.method == 'POST':
-        Post_title = request.POST['title']
-        Post_year = request.POST['year']
-        Post_conteudo = request.POST['conteudo']
-        Post_goleiro = request.POST['goleiro']
-        Post_defesa = request.POST['defesa']
-        Post_meio = request.POST['meio']
-        Post_ataque = request.POST['ataque']
-        Post_poster_url = request.POST['poster_url']
-        post = Post(title=Post_title, year=Post_year, conteudo=Post_conteudo, goleiro=Post_goleiro, defesa=Post_defesa, meio=Post_meio, ataque=Post_ataque,poster_url=Post_poster_url)
-        post.save()
-        return HttpResponseRedirect(
-            reverse('posts:detail', args=(post.id, )))
+        form = PostForm(request.POST)
+        if form.is_valid():
+            Post_title = request.POST['title']
+            Post_year = request.POST['year']
+            Post_conteudo = request.POST['conteudo']
+            Post_goleiro = request.POST['goleiro']
+            Post_defesa = request.POST['defesa']
+            Post_meio = request.POST['meio']
+            Post_ataque = request.POST['ataque']
+            Post_poster_url = request.POST['poster_url']
+            post = Post(title=Post_title, year=Post_year, conteudo=Post_conteudo, goleiro=Post_goleiro, defesa=Post_defesa, meio=Post_meio, ataque=Post_ataque,poster_url=Post_poster_url)
+            post.save()
+            return HttpResponseRedirect(
+                reverse('posts:detail', args=(post.id, )))
     else:
-        return render(request, 'posts/create.html', {})
+        form = PostForm()
+        context = {'form': form}
+        return render(request, 'posts/create.html', context)
     
 def update_elenco(request, Post_id):
     post = get_object_or_404(Post, pk=Post_id)
     if request.method == "POST":
-        post.title = request.POST['title']
-        post.year = request.POST['year']
-        post.conteudo = request.POST['conteudo']
-        post.goleiro = request.POST['goleiro']
-        post.defesa = request.POST['defesa']
-        post.meio = request.POST['meio']
-        post.ataque = request.POST['ataque']
-        post.poster_url = request.POST['poster_url']
-        post.save()
-        return HttpResponseRedirect(
-            reverse('posts:detail', args=(post.id, )))
-    context = {'Post': post}
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post.title = request.POST['title']
+            post.year = request.POST['year']
+            post.conteudo = request.POST['conteudo']
+            post.goleiro = request.POST['goleiro']
+            post.defesa = request.POST['defesa']
+            post.meio = request.POST['meio']
+            post.ataque = request.POST['ataque']
+            post.poster_url = request.POST['poster_url']
+            post.save()
+            return HttpResponseRedirect(
+                reverse('posts:detail', args=(post.id, )))
+    else:
+        form = PostForm(
+            initial={
+            'title': post.title,
+            'year': post.year,
+            'conteudo': post.conteudo,
+            'goleiro': post.goleiro,
+            'defesa': post.defesa,
+            'meio': post.meio,
+            'ataque': post.ataque,
+            'poster_url': post.poster_url,
+            })
+    context = {'Post': post, 'form': form}
     return render(request, 'posts/update.html', context)
+
     
 def delete_elenco(request, Post_id):
     post = get_object_or_404(Post, pk=Post_id)
