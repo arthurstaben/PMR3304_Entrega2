04ff94ff9dd81f8096446c11a14ad41e6f7fac16
João Arthur <arthurstaben@gmail.com>
2024-11-05 09:28:07 -0300

    Comentários Iniciais

---
+++ posts/migrations/0011_add_comments.py
@@ -0,0 +1,57 @@
+from django.db import migrations
+from django.utils import timezone
+
+def create_initial_comments(apps, schema_editor):
+    Post = apps.get_model('posts', 'Post')
+    User = apps.get_model('auth', 'User')
+    Comment = apps.get_model('posts', 'Comment')
+
+    post1 = Post.objects.get(id=1)
+    post3 = Post.objects.get(id=3)
+    user1 = User.objects.get(username="Lucas Araújo") 
+    user2 = User.objects.get(username="Charles Oliveira")
+    user3 = User.objects.get(username="Éverton Ribeiro")
+    user4 = User.objects.get(username="Ticiane Dois") 
+    user5 = User.objects.get(username="Artur Skull")   
+
+    Comment.objects.create(
+        post=post1,
+        author=user1,
+        text="Orgulho de ser nordestino. Bora Bahêa!",
+        data=timezone.now()
+    )
+    Comment.objects.create(
+        post=post1,
+        author=user2,
+        text="Esse time do Bahia era duríssimo. Bobô e Charles eram craques da pelota.",
+        data=timezone.now()
+    )
+    Comment.objects.create(
+        post=post1,
+        author=user3,
+        text="Que time era esse do esquadrão de aço. Lembro como se fosse hoje do 2x1 na Fonte Nova lotada!",
+        data=timezone.now()
+    )
+
+    Comment.objects.create(
+        post=post3,
+        author=user4,
+        text="Que final. Melhor jogo que vi na vida!",
+        data=timezone.now()
+    )
+    
+    Comment.objects.create(
+        post=post3,
+        author=user5,
+        text="Baita de uma final, melhor que assisti. Noite de brilho de Steven Gerrard.",
+        data=timezone.now()
+    )
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('posts', '0010_add_initial_posts'),  
+    ]
+
+    operations = [
+        migrations.RunPython(create_initial_comments),
+    ]
