75cb7284c90846ba454bb32d7d5c833432f70320
João Arthur <arthurstaben@gmail.com>
2024-11-05 09:43:47 -0300

    Comentários Iniciais

--- posts/migrations/0011_add_comments.py
+++ posts/migrations/0011_add_comments.py
@@ -1,5 +1,6 @@
 from django.db import migrations
 from django.utils import timezone
+from django.contrib.auth.hashers import make_password
 
 def create_initial_comments(apps, schema_editor):
     Post = apps.get_model('posts', 'Post')
@@ -11,27 +12,27 @@ def create_initial_comments(apps, schema_editor):
 
     user1, created = User.objects.get_or_create(username="Lucas Araújo")
     if created:
-        user1.set_password("password1")
+        user1.password = make_password("password1")
         user1.save()
         
     user2, created = User.objects.get_or_create(username="Charles Oliveira")
     if created:
-        user2.set_password("password2")
+        user2.password = make_password("password2")
         user2.save()
         
     user3, created = User.objects.get_or_create(username="Éverton Ribeiro")
     if created:
-        user3.set_password("password3")
+        user3.password = make_password("password3")
         user3.save()
         
     user4, created = User.objects.get_or_create(username="Ticiane Dois")
     if created:
-        user4.set_password("password4")
+        user4.password = make_password("password4")
         user4.save()
         
     user5, created = User.objects.get_or_create(username="Artur Skull")
     if created:
-        user5.set_password("password5")
+        user5.password = make_password("password5")
         user5.save()
 
     Comment.objects.create(
